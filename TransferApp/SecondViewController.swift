dc855c4afa18a7ebb645d698902aae611b43e61a
bristol <oro.eto.bristol@gmail.com>
2023-02-27 19:33:12 +0200

    Initial Commit

---
+++ TransferApp/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  TransferApp
+//
+//  Created by Apple M1 on 27.02.2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    @IBOutlet var dataTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ TransferApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TransferApp
+//
+//  Created by Apple M1 on 27.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var dataLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
